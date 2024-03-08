df3b70b73a9b049888489f634045a6c6da10aa5f
Nicos <hfnovember@hotmail.com>
2024-03-08 16:22:07 +0200

    Starting a treasure hunt.

--- js/list.js
+++ js/list.js
@@ -13,7 +13,7 @@ async function callApi(name) {
                 for (let i = 0; i < treasureHunts.length; i++) {
                     const item = treasureHunts[i];
                     // Using the template literals and string interpolation to iterate through the treasureHunts names and uuid (as uuid is different for each treasure hunt)
-                    const listItem = `<a href="https://codecyprus.org/th/api/start?player=${name}&app=SpartakDogushTeamApp&treasure-hunt-id=${item.uuid}" target="_blank">
+                    const listItem = `<a href="start.html?treasure-hunt-id=${item.uuid}" target="_blank">
                         <li class="list${i + 1}">${item.name}</li></a>`
                     treasureHuntElement.innerHTML += listItem;
                     // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
@@ -25,55 +25,55 @@ async function callApi(name) {
 
 // Using the addEventListener method instead of a usual function
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-treasureHuntElement.addEventListener('click', function (event){
-    // The condition that checks if the element that was clicked on is a <li> tag.
-    // source: https://www.w3schools.com/jsref/event_target.asp
-
-    if (event.target.tagName === 'LI'){
-        // Class list property
-        // source: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-        // I have used this property in conditions as otherwise I was returning the previous alert messages in list2 and list3
-        const classList = event.target.classList;
-
-        // source: https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
-        if (HasName)
-        {
-            const linkElement = event.target.closest('a[href]');
-
-            // Check if such an element exists
-            if (linkElement) {
-                // Get the href attribute of the closest <a> element
-                const link = linkElement.getAttribute('href');
-                clickedLink = link;
-                getTreasureHuntId(link);
-                // Now you have the link stored in the variable 'link'
-                // You can use it as needed, for example, displaying it in an alert:
-                console.log("Clicked link: " + link);
-            }
-
-            if (classList.contains('list1')) {
-                alert("Starting treasure hunt: UCLan Cyprus Science Day 2024");
-            }
-            if (classList.contains('list2')) {
-                alert("Starting treasure hunt: Code Cyprus 2024");
-            }
-            else if (classList.contains('list3')) {
-                alert("Starting treasure hunt: Sample treasure hunt");
-
-
-            }
-            else if (classList.contains('list4')) {
-                alert("Starting treasure hunt: A future treasure hunt");
-            }
-        }
-        else
-        {
-            alert("Fill in your name please");
-            event.preventDefault();
-        }
-
-    }
-});
+// treasureHuntElement.addEventListener('click', function (event){
+//     // The condition that checks if the element that was clicked on is a <li> tag.
+//     // source: https://www.w3schools.com/jsref/event_target.asp
+//
+//     if (event.target.tagName === 'LI'){
+//         // Class list property
+//         // source: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+//         // I have used this property in conditions as otherwise I was returning the previous alert messages in list2 and list3
+//         const classList = event.target.classList;
+//
+//         // source: https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
+//         if (HasName)
+//         {
+//             const linkElement = event.target.closest('a[href]');
+//
+//             // Check if such an element exists
+//             if (linkElement) {
+//                 // Get the href attribute of the closest <a> element
+//                 const link = linkElement.getAttribute('href');
+//                 clickedLink = link;
+//                 getTreasureHuntId(link);
+//                 // Now you have the link stored in the variable 'link'
+//                 // You can use it as needed, for example, displaying it in an alert:
+//                 console.log("Clicked link: " + link);
+//             }
+//
+//             if (classList.contains('list1')) {
+//                 alert("Starting treasure hunt: UCLan Cyprus Science Day 2024");
+//             }
+//             if (classList.contains('list2')) {
+//                 alert("Starting treasure hunt: Code Cyprus 2024");
+//             }
+//             else if (classList.contains('list3')) {
+//                 alert("Starting treasure hunt: Sample treasure hunt");
+//
+//
+//             }
+//             else if (classList.contains('list4')) {
+//                 alert("Starting treasure hunt: A future treasure hunt");
+//             }
+//         }
+//         else
+//         {
+//             alert("Fill in your name please");
+//             event.preventDefault();
+//         }
+//
+//     }
+// });
 
 
 function updateLinks(name) {
