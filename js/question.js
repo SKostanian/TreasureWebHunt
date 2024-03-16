c139103316875b596fe34c2d49b0e966b3761189
sparenkov <sparenkov@uclan.ac.uk>
2024-03-16 23:56:10 +0300

    question and start page design

--- js/question.js
+++ js/question.js
@@ -14,6 +14,23 @@ function getQuestion() {
                 let questionText = jsonData.questionText;
                 questionElement.innerHTML = questionText;
                 console.log(questionElement)
+                if (jsonData.canBeSkipped)
+                {
+                    const answerContainer = document.querySelector('.answer_container');
+
+// Create the first button
+                    const button1 = document.createElement('button');
+                    button1.textContent = 'Button 1'; // Set the button text
+
+// Create the second button
+                    const button2 = document.createElement('button');
+                    button2.textContent = 'Button 2'; // Set the button text
+
+// Append the buttons to the answer container
+                    answerContainer.appendChild(button1);
+                    answerContainer.appendChild(button2);
+                }
+
             }
 
 
@@ -34,6 +51,8 @@ function answer() {
         .then(response => response.json())
         .then(jsonData => {
             answerElement.value = "";
+            console.log(sessionID);
+            console.log(jsonData.canBeSkipped);
             console.log(jsonData);
             if (jsonData.status === "OK") {
 
@@ -49,6 +68,9 @@ function answer() {
                 else {
                     alert(jsonData.message);
                 }
+                if (jsonData.canBeSkipped) {
+                    alert(jsonData.canBeSkipped);
+                }
             }
 
 
@@ -59,4 +81,34 @@ function answer() {
 
 function skip() {
 
+    let sessionID = getCookie("sessionID");
+
+    fetch(`https://codecyprus.org/th/api/skip?session=${sessionID}`)
+        .then(response => response.json())
+        .then(jsonData => {
+            answerElement.value = "";
+            console.log(sessionID);
+
+            console.log(jsonData);
+            if (jsonData.status === "OK") {
+
+                if (jsonData.completed) {
+                    //TODO - Move to the leaderboard.
+                    alert("TODO - Move to the leaderboard")
+                }
+
+                if (jsonData.correct) {
+                    alert(jsonData.message);
+                    getQuestion();
+                }
+                else {
+                    alert(jsonData.message);
+                    getQuestion();
+                }
+            }
+
+
+            // Now you can use the questionText variable as needed
+        })
+        .catch(error => console.error('Error fetching data:', error));
 }
